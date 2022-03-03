ce2b3f1e8ffbe8963f353c9b44dc22f593d13281
archana-6 <93028326+archana-6@users.noreply.github.com>
2022-03-03 16:46:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import REACT from "react"
+import REACTDOM from "react-dom"
+
+REACTDOM.render(<div>
+<img src="https://m.media-amazon.com/images/I/61q6x-ll5FL._SL1000_.jpg"></img>
+<p>Lenovo Ideapad 3 AMD Ryzen 5 5500U 15.6" FHD Thin & Light Laptop (8GB/512GB</p>
+<button>ADD TO CART</button></div>,document.getElementById("root"))
+REACTDOM.render(<div>
+    <img src="https://m.media-amazon.com/images/I/61q6x-ll5FL._SL1000_.jpg"></img>
+    <p>Lenovo Ideapad 3 AMD Ryzen 5 5500U 15.6" FHD Thin & Light Laptop (8GB/512GB</p>
+    <button>ADD TO CART</button></div>,document.getElementById("root1"))
+    REACTDOM.render(<div>
+        <img src="https://m.media-amazon.com/images/I/61q6x-ll5FL._SL1000_.jpg"></img>
+        <p>Lenovo Ideapad 3 AMD Ryzen 5 5500U 15.6" FHD Thin & Light Laptop (8GB/512GB</p>
+        <button >ADD TO CART</button></div>,document.getElementById("root2"))
